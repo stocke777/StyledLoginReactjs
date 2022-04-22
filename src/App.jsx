ae4b48252bbe8ab55a0fb28f19b13a4b8f4841cd
stocke777 <deshwaljaivardhan@gmail.com>
2022-04-22 17:35:02 +0530

    all files added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Pages
+import Home from './pages/home.jsx' 
+
+//  Styled Components
+import {StyledContainer} from './components/Styles.js'
+
+function App() {
+  return (
+    <StyledContainer>
+      <Home />
+    </StyledContainer>
+  );
+}
+
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import {StylesTitle, StylesSubTitle} from './../components/Styles'
+
+const Home = () => {
+    return (
+        <div>
+            <StylesTitle size={65}>
+                Welcome
+            </StylesTitle>
+            <StylesSubTitle size={25}>
+                Feel free to explore.
+            </StylesSubTitle>
+        </div>
+    )
+}
+
+export default Home
